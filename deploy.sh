2ae38222f41cb4a86a81fbbf92ae345120b5278a
Alexey Bystrov <triple0zero@gmail.com>
2020-07-19 10:57:42 +0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com 'docker-compose --version &&
   cd dreamapp-cd && \
+  echo "UPDATE FILES FROM REPO" &&
   git pull &&
   pwd &&
   echo "STOP DOCKER COMPOSE" &&
   sudo docker-compose down &&
   echo "START DOCKER COMPOSE" &&
   sudo docker-compose up -d &&
-  echo "CHECK DC PS" &&
+  echo "" &&
+  echo "CHECK COMPOSE PS" &&
   sudo docker-compose ps &&
+  echo "" &&
   echo "CHECK DOCKER PS" &&
   sudo docker ps
   '
