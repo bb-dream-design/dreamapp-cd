3c585d3943d7eff1c338d33e39045b1c30a389dd
Alexey Bystrov <triple0zero@gmail.com>
2020-07-19 10:45:54 +0300

    deploy.sh script fix add separate folder for cadence compose

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
-ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com 'docker-compose --version &&
- cd dreamapp-cd && \
- git pull &&
- pwd &&
- echo "STOP DOCKER COMPOSE" &&
- sudo docker-compose down &&
- echo "START DOCKER COMPOSE" &&
- sudo docker-compose up -d &&
- echo "CHECK DC PS" &&
- sudo docker-compose ps &&
- echo "CHECK DOCKER PS" &&
- sudo docker ps'
+ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com
+docker-compose --version &&
+cd dreamapp-cd && \
+git pull &&
+pwd &&
+echo "STOP DOCKER COMPOSE" &&
+sudo docker-compose down &&
+echo "START DOCKER COMPOSE" &&
+sudo docker-compose up -d &&
+echo "CHECK DC PS" &&
+sudo docker-compose ps &&
+echo "CHECK DOCKER PS" &&
+sudo docker ps
