863621dee1826024a5b568e0735f362a76a99d38
alexey bystrov <triple0zero@gmail.com>
2020-07-23 15:25:52 +0300

    fix &&

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com 'do
   cd dreamapp-cd && \
   echo "UPDATE FILES FROM REPO" &&
   git pull &&
-  sudo docker pull dream20200716/dream-design-app:latest
+  sudo docker pull dream20200716/dream-design-app:latest &&
   pwd &&
   echo "STOP DOCKER COMPOSE" &&
   sudo docker-compose down &&
