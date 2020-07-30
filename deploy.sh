3f6915942748e470a3a05031f457ad3a064fee06
Ubuntu <ubuntu@ip-172-31-39-89.us-east-2.compute.internal>
2020-07-30 10:00:18 +0300

    dockerhost is local ip

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,22 @@ ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com 'do
   sudo docker pull dream20200716/dream-ui:latest &&
   pwd &&
   #export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk "{ print  }" | cut -f2 -d: | head -n1) &&
-  #export DOCKERHOST=$(hostname -I | cut -d ' ' -f1)
+  export HOSTIP=$(hostname -I | cut -d ' ' -f1) &&
   #echo "DOCKERHOST=" &&
   #echo $DOCKERHOST &&
+  echo "HOSTIP=" &&
+  echo $HOSTIP &&
+  echo "===================" &&
   echo "STOP DOCKER COMPOSE" &&
   sudo docker-compose down &&
+  echo "===================" &&
   echo "START DOCKER COMPOSE" &&
   sudo docker-compose up -d &&
   echo "" &&
   echo "CHECK COMPOSE PS" &&
   sudo docker-compose ps &&
   echo "" &&
+  echo "===================" &&
   echo "CHECK DOCKER PS" &&
   sudo docker ps
   '
