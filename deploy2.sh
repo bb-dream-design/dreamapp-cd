62c6fca9c1978784b1829b14875cb28c54f21955
17618368 <bystrov.a.e@sberbank.ru>
2020-07-30 12:31:48 +0300

    exec deply from host folder fix2 deploy2 test export

--- deploy2.sh
+++ deploy2.sh
@@ -8,12 +8,12 @@ sudo docker pull dream20200716/dream-ui:latest && \
 pwd && \
 #export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk "{ print  }" | cut -f2 -d: | head -n1) &&
 export HOSTIP=$(hostname -I | cut -d ' ' -f1) && \
-export QQQQ="IT IS MY VAR" && \
+#export QQQQ="IT IS MY VAR" && \
 #echo "DOCKERHOST=" && \
 #echo $DOCKERHOST && \
 echo "HOSTIP=" && \
 echo $HOSTIP && \
-echo $QQQQ && \
+#echo $QQQQ && \
 echo "===================" && \
 echo "STOP DOCKER COMPOSE" && \
 sudo docker-compose down && \
