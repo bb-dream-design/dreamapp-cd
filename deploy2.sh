8a47cd76f61fa058b3fbbb2cab9ce957fd69a49f
17618368 <bystrov.a.e@sberbank.ru>
2020-07-30 12:50:39 +0300

    pretty log print

--- deploy2.sh
+++ deploy2.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/bash
+echo "=========| START DEPOLOY |==========" && \
+echo "=========| CHECK VERSION |==========" && \
 docker-compose --version && \
 #  cd dreamapp-cd && \
 #  echo "UPDATE FILES FROM REPO" &&
 #  git pull &&
+echo "" && \
+echo "=========| PULLING LATEST VERSION |==========" && \
 sudo docker pull dream20200716/dream-design-app:latest && \
 sudo docker pull dream20200716/dream-ui:latest && \
+echo "" && \
+echo "=========| CHECK WHERE AM I |==========" && \
 pwd && \
+echo "" && \
+echo "=========| GET LOCAL HOST IP FOR PROXY_PASS REQUESTS |==========" && \
 #export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk "{ print  }" | cut -f2 -d: | head -n1) &&
+#echo "DOCKERHOST= $DOCKERHOST" && \
 export HOSTIP=$(hostname -I | cut -d ' ' -f1) && \
-#export QQQQ="IT IS MY VAR" && \
-#echo "DOCKERHOST=" && \
-#echo $DOCKERHOST && \
+echo "HOSTIP = $HOSTIP" && \
+echo "" && \
+echo "=========| WRITE LOCAL HOST IP INTO .ENV |==========" && \
 echo "HOSTIP=$HOSTIP" > .env && \
-#echo $QQQQ && \
-echo "===================" && \
-echo "STOP DOCKER COMPOSE" && \
+echo "" && \
+echo "=========| STOP DOCKER COMPOSE |==========" && \
 sudo docker-compose down && \
-echo "===================" && \
-echo "START DOCKER COMPOSE" && \
+echo "" && \
+echo "=========| START DOCKER COMPOSE |==========" && \
 sudo docker-compose up -d && \
 echo "" && \
-echo "CHECK COMPOSE PS" &&
+echo "=========| CHECK COMPOSE PS |==========" && \
 sudo docker-compose ps && \
 echo "" && \
-echo "===================" && \
-echo "CHECK DOCKER PS" && \
+echo "=========| CHECK DOCKER PS |==========" && \
 sudo docker ps
