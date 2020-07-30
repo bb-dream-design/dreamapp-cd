d27807235a4499d7f5b9ba156f687cfd125a7c76
Alexey Bystrov <triple0zero@gmail.com>
2020-07-30 10:32:18 +0300

    test with new export var

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com 'do
   pwd &&
   #export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk "{ print  }" | cut -f2 -d: | head -n1) &&
   export HOSTIP=$(hostname -I | cut -d ' ' -f1) &&
+  export QQQQ=$(hostname -I | cut -d ' ' -f1) &&
   #echo "DOCKERHOST=" &&
   #echo $DOCKERHOST &&
   echo "HOSTIP=" &&
   echo $HOSTIP &&
+  echo $QQQQ &&
   echo "===================" &&
   echo "STOP DOCKER COMPOSE" &&
   sudo docker-compose down &&
