a4e7e1d76ab78b2b9f0c15fd64b47bd8461438d7
Alexey Bystrov <triple0zero@gmail.com>
2020-07-30 08:54:36 +0300

    add dockerhost for ui fix

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ ssh -i aws-dream.pem ubuntu@ec2-3-15-210-253.us-east-2.compute.amazonaws.com 'do
   sudo docker pull dream20200716/dream-ui:latest &&
   pwd &&
   export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk "{ print  }" | cut -f2 -d: | head -n1) &&
+  echo "DOCKERHOST=" &&
+  echo $DOCKERHOST &&
   echo "STOP DOCKER COMPOSE" &&
   sudo docker-compose down &&
   echo "START DOCKER COMPOSE" &&
